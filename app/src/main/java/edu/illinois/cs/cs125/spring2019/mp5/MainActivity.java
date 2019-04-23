d1c5539bb3b50ba78dc840f231d4f4e36de2adac
lavanyasrivastava <lavanya6@illinois.edu>
2019-04-23 16:15:22 -0500

    buttonx2

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/mp5/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/mp5/MainActivity.java
@@ -26,12 +26,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MP5:Main";
 
     /** Request queue for our network requests. */
-    private static RequestQueue requestQueue;
+    //private static RequestQueue requestQueue;
 
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestQueue = Volley.newRequestQueue(this);
+        //requestQueue = Volley.newRequestQueue(this);
 
         setContentView(R.layout.activity_main);
         final TextView hi = findViewById(R.id.textView);
