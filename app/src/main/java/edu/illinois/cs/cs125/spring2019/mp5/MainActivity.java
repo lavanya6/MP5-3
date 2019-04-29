f8acf6f5c4f5876f9721190c436db02192ce32a3
malikabasu <malikab2@illinois.edu>
2019-04-29 01:28:33 -0500

    Nearly finished the app!

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/mp5/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/mp5/MainActivity.java
@@ -42,6 +42,7 @@ import org.json.JSONArray;
 import com.google.gson.JsonObject;
 import java.util.ArrayList;
 import android.widget.ArrayAdapter;
+import android.text.method.ScrollingMovementMethod;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -63,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         hi = findViewById(R.id.textView);
         hi.setVisibility(View.GONE);
-        specialty = findViewById(R.id.speciality);
-        sp = specialty.getText().toString().toLowerCase().trim();
+        specialty = (EditText) findViewById(R.id.speciality);
+        sp = specialty.getText().toString().toLowerCase();
         final Button findDoctor = findViewById(R.id.doctor);
         findDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,10 +104,147 @@ public class MainActivity extends AppCompatActivity {
     }
     void apiCallDone(final JSONObject response) {
         try {
-            Log.d(TAG, response.toString(2));
             // Example of how to pull a field off the returned JSON object
-            Log.i(TAG, response.get("data").toString());
-            hi.setText(response.get("data").toString());
+
+            JSONObject responseJSON = new JSONObject(response.toString());
+            System.out.print(response.toString());
+            JSONArray dataJSON = responseJSON.getJSONArray("data");
+
+            //1Doctor
+            JSONObject fDoctorJSON = dataJSON.getJSONObject(0);
+            JSONArray fDoctorPractices = fDoctorJSON.getJSONArray("practices");
+            JSONObject fDoctorUn = fDoctorPractices.getJSONObject(0);
+            Object fDoctorName = fDoctorUn.get("name");
+            String fDoctorNameString = fDoctorName.toString();
+            JSONObject fDoctorOffice = fDoctorUn.getJSONObject("visit_address");
+            String fDoctorOfficeAddress = fDoctorOffice.get("street").toString() + "," + fDoctorOffice.get("city").toString() + "," + fDoctorOffice.get("state").toString() + "-" + fDoctorOffice.get("zip").toString();
+            JSONArray fDoctorP = fDoctorUn.getJSONArray("phones");
+            JSONObject fDoctorPhone = fDoctorP.getJSONObject(0);
+            String fDoctorNumber = fDoctorPhone.get("number").toString();
+
+            //2doctor
+            JSONObject sDoctorJSON = dataJSON.getJSONObject(1);
+            JSONArray sDoctorPractices = sDoctorJSON.getJSONArray("practices");
+            JSONObject sDoctorUn = sDoctorPractices.getJSONObject(0);
+            Object sDoctorName = sDoctorUn.get("name");
+            String sDoctorNameString = sDoctorName.toString();
+            JSONObject sDoctorOffice = sDoctorUn.getJSONObject("visit_address");
+            String sDoctorOfficeAddress = sDoctorOffice.get("street").toString() + "," + sDoctorOffice.get("city").toString() + "," + sDoctorOffice.get("state").toString() + "-" + sDoctorOffice.get("zip").toString();
+            JSONArray sDoctorP = sDoctorUn.getJSONArray("phones");
+            JSONObject sDoctorPhone = sDoctorP.getJSONObject(0);
+            String sDoctorNumber = sDoctorPhone.get("number").toString();
+
+            //3doctor
+            JSONObject tDoctorJSON = dataJSON.getJSONObject(2);
+            JSONArray tDoctorPractices = tDoctorJSON.getJSONArray("practices");
+            JSONObject tDoctorUn = tDoctorPractices.getJSONObject(0);
+            Object tDoctorName = tDoctorUn.get("name");
+            String tDoctorNameString = tDoctorName.toString();
+            JSONObject tDoctorOffice = tDoctorUn.getJSONObject("visit_address");
+            String tDoctorOfficeAddress = tDoctorOffice.get("street").toString() + "," + tDoctorOffice.get("city").toString() + "," + tDoctorOffice.get("state").toString() + "-" + tDoctorOffice.get("zip").toString();
+            JSONArray tDoctorP = tDoctorUn.getJSONArray("phones");
+            JSONObject tDoctorPhone = tDoctorP.getJSONObject(0);
+            String tDoctorNumber = tDoctorPhone.get("number").toString();
+
+            //4doctor
+            JSONObject bDoctorJSON = dataJSON.getJSONObject(3);
+            JSONArray bDoctorPractices = bDoctorJSON.getJSONArray("practices");
+            JSONObject bDoctorUn = bDoctorPractices.getJSONObject(0);
+            Object bDoctorName = bDoctorUn.get("name");
+            String bDoctorNameString = bDoctorName.toString();
+            JSONObject bDoctorOffice = bDoctorUn.getJSONObject("visit_address");
+            String bDoctorOfficeAddress = bDoctorOffice.get("street").toString() + "," + bDoctorOffice.get("city").toString() + "," + bDoctorOffice.get("state").toString() + "-" + bDoctorOffice.get("zip").toString();
+            JSONArray bDoctorP = bDoctorUn.getJSONArray("phones");
+            JSONObject bDoctorPhone = bDoctorP.getJSONObject(0);
+            String bDoctorNumber = bDoctorPhone.get("number").toString();
+
+            //5doctor
+            JSONObject pentDoctorJSON = dataJSON.getJSONObject(4);
+            JSONArray pentDoctorPractices = pentDoctorJSON.getJSONArray("practices");
+            JSONObject pentDoctorUn = pentDoctorPractices.getJSONObject(0);
+            Object pentDoctorName = pentDoctorUn.get("name");
+            String pentDoctorNameString = pentDoctorName.toString();
+            JSONObject pentDoctorOffice = pentDoctorUn.getJSONObject("visit_address");
+            String pentDoctorOfficeAddress = pentDoctorOffice.get("street").toString() + "," + pentDoctorOffice.get("city").toString() + "," + pentDoctorOffice.get("state").toString() + "-" + pentDoctorOffice.get("zip").toString();
+            JSONArray pentDoctorP = pentDoctorUn.getJSONArray("phones");
+            JSONObject pentDoctorPhone = pentDoctorP.getJSONObject(0);
+            String pentDoctorNumber = pentDoctorPhone.get("number").toString();
+
+            //6doctor
+            JSONObject hexDoctorJSON = dataJSON.getJSONObject(5);
+            JSONArray hexDoctorPractices = hexDoctorJSON.getJSONArray("practices");
+            JSONObject hexDoctorUn = hexDoctorPractices.getJSONObject(0);
+            Object hexDoctorName = hexDoctorUn.get("name");
+            String hexDoctorNameString = hexDoctorName.toString();
+            JSONObject hexDoctorOffice = hexDoctorUn.getJSONObject("visit_address");
+            String hexDoctorOfficeAddress = hexDoctorOffice.get("street").toString() + "," + hexDoctorOffice.get("city").toString() + "," + hexDoctorOffice.get("state").toString() + "-" + hexDoctorOffice.get("zip").toString();
+            JSONArray hexDoctorP = hexDoctorUn.getJSONArray("phones");
+            JSONObject hexDoctorPhone = hexDoctorP.getJSONObject(0);
+            String hexDoctorNumber = hexDoctorPhone.get("number").toString();
+
+            //7doctor
+            JSONObject heptDoctorJSON = dataJSON.getJSONObject(6);
+            JSONArray heptDoctorPractices = heptDoctorJSON.getJSONArray("practices");
+            JSONObject heptDoctorUn = heptDoctorPractices.getJSONObject(0);
+            Object heptDoctorName = heptDoctorUn.get("name");
+            String heptDoctorNameString = heptDoctorName.toString();
+            JSONObject heptDoctorOffice = heptDoctorUn.getJSONObject("visit_address");
+            String heptDoctorOfficeAddress = heptDoctorOffice.get("street").toString() + "," + heptDoctorOffice.get("city").toString() + "," + heptDoctorOffice.get("state").toString() + "-" + heptDoctorOffice.get("zip").toString();
+            JSONArray heptDoctorP = heptDoctorUn.getJSONArray("phones");
+            JSONObject heptDoctorPhone = heptDoctorP.getJSONObject(0);
+            String heptDoctorNumber = heptDoctorPhone.get("number").toString();
+
+            //8doctor
+            JSONObject octDoctorJSON = dataJSON.getJSONObject(7);
+            JSONArray octDoctorPractices = octDoctorJSON.getJSONArray("practices");
+            JSONObject octDoctorUn = octDoctorPractices.getJSONObject(0);
+            Object octDoctorName = octDoctorUn.get("name");
+            String octDoctorNameString = octDoctorName.toString();
+            JSONObject octDoctorOffice = octDoctorUn.getJSONObject("visit_address");
+            String octDoctorOfficeAddress = octDoctorOffice.get("street").toString() + "," + octDoctorOffice.get("city").toString() + "," + octDoctorOffice.get("state").toString() + "-" + octDoctorOffice.get("zip").toString();
+            JSONArray octDoctorP = octDoctorUn.getJSONArray("phones");
+            JSONObject octDoctorPhone = octDoctorP.getJSONObject(0);
+            String octDoctorNumber = octDoctorPhone.get("number").toString();
+
+            //9doctor
+            JSONObject nonDoctorJSON = dataJSON.getJSONObject(8);
+            JSONArray  nonDoctorPractices = nonDoctorJSON.getJSONArray("practices");
+            JSONObject nonDoctorUn = nonDoctorPractices.getJSONObject(0);
+            Object nonDoctorName = nonDoctorUn.get("name");
+            String nonDoctorNameString = nonDoctorName.toString();
+            JSONObject nonDoctorOffice = nonDoctorUn.getJSONObject("visit_address");
+            String nonDoctorOfficeAddress = nonDoctorOffice.get("street").toString() + "," + nonDoctorOffice.get("city").toString() + "," + nonDoctorOffice.get("state").toString() + "-" + nonDoctorOffice.get("zip").toString();
+            JSONArray nonDoctorP = nonDoctorUn.getJSONArray("phones");
+            JSONObject nonDoctorPhone = nonDoctorP.getJSONObject(0);
+            String nonDoctorNumber = nonDoctorPhone.get("number").toString();
+
+            //10doctor
+            JSONObject decDoctorJSON = dataJSON.getJSONObject(9);
+            JSONArray  decDoctorPractices = decDoctorJSON.getJSONArray("practices");
+            JSONObject decDoctorUn = decDoctorPractices.getJSONObject(0);
+            Object decDoctorName = decDoctorUn.get("name");
+            String decDoctorNameString = decDoctorName.toString();
+            JSONObject decDoctorOffice = decDoctorUn.getJSONObject("visit_address");
+            String decDoctorOfficeAddress = decDoctorOffice.get("street").toString() + "," + decDoctorOffice.get("city").toString() + "," + decDoctorOffice.get("state").toString() + "-" + decDoctorOffice.get("zip").toString();
+            JSONArray decDoctorP = decDoctorUn.getJSONArray("phones");
+            JSONObject decDoctorPhone = decDoctorP.getJSONObject(0);
+            String decDoctorNumber = decDoctorPhone.get("number").toString();
+
+
+            hi.setText("Results" + "\n" + "\n" + "1. " + fDoctorNameString + "\n" + fDoctorOfficeAddress + "\n" + fDoctorNumber
+                        + "\n" + "\n" + "2. " + sDoctorNameString + "\n" + sDoctorOfficeAddress + "\n" + sDoctorNumber
+                        + "\n" + "\n" + "3. " + tDoctorNameString + "\n" + tDoctorOfficeAddress + "\n" + tDoctorNumber
+                        + "\n" + "\n" + "4. " + bDoctorNameString + "\n" + bDoctorOfficeAddress + "\n" + bDoctorNumber
+                        + "\n" + "\n" + "5. " + pentDoctorNameString + "\n" + pentDoctorOfficeAddress + "\n" + pentDoctorNumber
+                        + "\n" + "\n" + "6. " + hexDoctorNameString + "\n" + hexDoctorOfficeAddress + "\n" + hexDoctorNumber
+                        + "\n" + "\n" + "7. " + heptDoctorNameString + "\n" + heptDoctorOfficeAddress + "\n" + heptDoctorNumber
+                        + "\n" + "\n" + "8. " + octDoctorNameString + "\n" + octDoctorOfficeAddress + "\n" + octDoctorNumber
+                        + "\n" + "\n" + "9. " + nonDoctorNameString + "\n" + nonDoctorOfficeAddress + "\n" + nonDoctorNumber
+                        + "\n" + "\n" + "10. " + decDoctorNameString + "\n" + decDoctorOfficeAddress + "\n" + decDoctorNumber);
+            hi.setTextSize(18);
+            hi.setMovementMethod(new ScrollingMovementMethod());
+            Log.d(TAG, sDoctorNameString);
+
         } catch (JSONException ignored) { }
     }
 }
